ba9c8956219c4cfdb04fe4ce20815440673c4c29
Nuwan Maduranga <maduranga.k@teckleo.com>
2019-09-19 21:45:00 +0530

    Add project files.

---
+++ SignalRPrivateChat/ChatHelper/UserDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRPrivateChat.ChatHelper
+{
+    public class UserDetail
+    {
+        public int ID { get; set; }
+        public string ConnectionId { get; set; }
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+    }
+}
